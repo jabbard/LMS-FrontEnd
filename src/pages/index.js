36d80a26daddc6c07e2ea1f2159388a0b71e0a84
pradhumnadhungana <pradhumnadhungana@gmail.com>
2025-08-21 17:33:36 +1000

    layout setup with header done

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as HomePage } from './home/HomePage';
+export { default as DashboardPage } from './dashboard/DashboardPage';
+export { default as ReviewsPage } from './reviews/ReviewsPage';
+export { default as EditBookPage } from './books/EditBookPage';
+// export { default as NotFoundPage } from './NotFoundPage';
