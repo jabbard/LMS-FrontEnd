33458a8b0a65bcabcae01a861d6c11d7f69fbe71
pradhumnadhungana <pradhumnadhungana@gmail.com>
2025-08-21 17:33:06 +1000

    layout setup with header done

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { 
-    HomePage, 
-    ReviewsPage,
-    DashboardPage,
-    EditBookPage 
+import {
+  HomePage,
+  ReviewsPage,
+  DashboardPage,
+  EditBookPage,
 } from "../pages/index";
+import DefaultLayout from "../components/layouts/DefaultLayout";
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
-
         {/* public pages */}
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <DefaultLayout>
+              <HomePage />
+            </DefaultLayout>
+          }
+        />
 
         {/* private pages */}
         <Route path="/reviews" element={<ReviewsPage />} />
-
       </Routes>
     </div>
   );
-}
+};
 
 export default AppRoutes;
