bbfcec4e712254ef1795dd71a3c0ad1da487956a
pradhumnadhungana <pradhumnadhungana@gmail.com>
2025-09-01 22:17:51 +1000

    user layout started

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -13,16 +13,26 @@ import {
   UserPage 
 } from "../pages/index";
 import DefaultLayout from "../components/layouts/DefaultLayout";
+import UserLayout from "../components/layouts/UserLayout";
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
+        {/* Public Pages */}
         <Route path="/" element={<DefaultLayout/>}>
           <Route index element={<HomePage />} />
           <Route path="signup" element={<SignUpPage/>} />
           <Route path="login" element={<LoginPage/>} />
           <Route path="forget-password" element={< ForgetPassword />} />
+        </Route>
+
+        {/* Protected Pages */}
+        <Route path="/user" element={<UserLayout/>}>
+          <Route index element={< DashboardPage />} />
+          {/* <Route path="signup" element={<SignUpPage/>} />
+          <Route path="login" element={<LoginPage/>} />
+          <Route path="forget-password" element={< ForgetPassword />} /> */}
         </Route>
       </Routes>
     </div>
