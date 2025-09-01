fe09553a0de5fed91e5c5170af2166aba94e9eb3
pradhumnadhungana <pradhumnadhungana@gmail.com>
2025-09-01 22:53:45 +1000

    user layout completed

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,7 +10,10 @@ import {
   NewBookPage,
   DashboardPage, 
   ReviewsPage, 
-  UserPage 
+  UserPage,
+  Books,
+  Profile,
+  BorrowPage 
 } from "../pages/index";
 import DefaultLayout from "../components/layouts/DefaultLayout";
 import UserLayout from "../components/layouts/UserLayout";
@@ -30,9 +33,15 @@ const AppRoutes = () => {
         {/* Protected Pages */}
         <Route path="/user" element={<UserLayout/>}>
           <Route index element={< DashboardPage />} />
-          {/* <Route path="signup" element={<SignUpPage/>} />
-          <Route path="login" element={<LoginPage/>} />
-          <Route path="forget-password" element={< ForgetPassword />} /> */}
+          <Route path="books" element={<Books/>} />
+          <Route path="new-book" element={<NewBookPage/>} />
+          <Route path="edit-book" element={<EditBookPage/>} />
+          <Route path="book-landing" element={<BookLandingPage/>} />
+          <Route path="reviews" element={<ReviewsPage/>} />
+          <Route path="user-list" element={<UserPage/>} />
+          <Route path="profile" element={<Profile/>} />
+          <Route path="borrow" element={<BorrowPage/>} />
+          {/* <Route path="borrow" element={<  />} /> */}
         </Route>
       </Routes>
     </div>
